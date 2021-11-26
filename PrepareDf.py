5a84ecd87d779af0e773e5aa820272e7605b20a4
Akshay Kekuda <kekuak@d-ctapp8dc01.safeautonet.net>
2021-11-26 08:50:50 -0500

    Increased thresh for negative call category determination

--- PrepareDf.py
+++ PrepareDf.py
@@ -31,7 +31,7 @@ def prepare_score_df(path_to_p, workgroup):
     # change baseline score range accordingly
     # overall score between 0 and 1
     score_df['CombinedPercentileScore'] = (calls_df.CombinedPercentileScore[::10]/100).astype(float).round(4)
-    score_df['Category'] = (score_df['CombinedPercentileScore'] < 0.6).apply(lambda x: int(x))
+    score_df['Category'] = (score_df['CombinedPercentileScore'] < 0.75).apply(lambda x: int(x))
     # overall score between 0 and 100
     # score_df['CombinedPercentileScore'] = (calls_df.CombinedPercentileScore[::10]).astype(float).round(2)
     # score_df['Category'] = (score_df['CombinedPercentileScore'] > 75).apply(lambda x: int(x))
